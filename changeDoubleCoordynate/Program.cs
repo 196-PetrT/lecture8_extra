0d87692d8823b40073057dff90a3e8c934743858
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-14 21:09:33 +0300

    принимаем значения парных координат и х2

---
+++ changeDoubleCoordynate/Program.cs
@@ -0,0 +1,27 @@
+﻿// наша задача увеличить координаты ровно в два раза
+// узнаем какие правила 
+
+
+using System.Linq;
+
+string text = "(1,2) (3,4) (5,6) (7,8)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ") // разделение по пробелу 
+                .Select(item => item.Split(','))
+                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1]))) // сделаем выборку из текущего массива 
+                // .Where(e => e.x % 2 == 0) // чтобы сделать проверку условия
+                .Select(point => (point.x * 2, point.y * 2))
+                .ToArray(); // превратить в явный массив
+
+for (int i = 0; i < data.Length; i++)
+{
+    // Console.WriteLine(data[i].Item1*2);
+    Console.Write(data[i]);
+    
+    Console.WriteLine();
+}
+
