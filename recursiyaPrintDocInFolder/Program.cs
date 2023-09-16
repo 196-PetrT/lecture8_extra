779756ac14c9560b8b0f21bc9e4364965cc31d40
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-16 20:36:44 +0300

    добавил задачи лекции 8

---
+++ recursiyaPrintDocInFolder/Program.cs
@@ -0,0 +1,21 @@
+﻿// Как посмотреть содержимое папки?
+// Решение некоторых задач
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        System.Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+
+string path = @"C:\Downloaded"; // без @ выдает ошибку
+DirectoryInfo di = new DirectoryInfo(path);
+Console.WriteLine(di.CreationTime);
+CatalogInfo(path);
